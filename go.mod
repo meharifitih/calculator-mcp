c59973a1fca05f7557f2ecf3aa56544727b6b563
Mehari Fitihamlak <meharifitih@gmail.com>
2025-11-03 21:39:29 +0300

    added calcualte tool and tool handdler

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require github.com/modelcontextprotocol/go-sdk v1.1.0
 
 require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/google/jsonschema-go v0.3.0 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
--- server.go
+++ server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
@@ -15,12 +17,48 @@ const (
 	serverVersion = "1.0.0"
 )
 
+// CalculateParams defines the parameters for the calculate tool.
+type CalculateParams struct {
+	Operation string  `json:"operation" jsonschema:"operation to be performed on the numbers"`
+	Num1      float32 `json:"num1" jsonschema:"first number"`
+	Num2      float32 `json:"num2" jsonschema:"second number"`
+}
+
+func (p CalculateParams) Validate() error {
+	return validation.ValidateStruct(&p,
+		validation.Field(&p.Operation,
+			validation.Required,
+			validation.In("add", "subtract", "multiply", "divide"),
+		),
+		validation.Field(&p.Num1, validation.Required),
+		validation.Field(&p.Num2,
+			validation.Required,
+			validation.By(func(value interface{}) error {
+				if p.Operation == "divide" && p.Num2 == 0 {
+					return errors.New("cannot divide by zero")
+				}
+				return nil
+			}),
+		),
+	)
+}
+
+// CalculateResult defines the result for the calculate tool.
+type CalculateResult struct {
+	Result float32 `json:"result" jsonschema:"result of the operation"`
+}
+
 func createMCPServer() *mcp.Server {
 	server := mcp.NewServer(&mcp.Implementation{
 		Name:    serverName,
 		Version: serverVersion},
 		nil)
 	log.Printf("Initializing MCP server: %s v%s", serverName, serverVersion)
+
+	mcp.AddTool(server, &mcp.Tool{
+		Name:        "calculate",
+		Description: "Perform basic mathematical operations like add, subtract, multiply, and divide",
+	}, handleCalculate)
 
 	return server
 }
@@ -37,7 +75,7 @@ func main() {
 	switch transport {
 	case "stdio":
 		if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
-			log.Fatal("Stdio server error: %v", err)
+			log.Fatalf("Stdio server error: %v", err)
 		}
 	case "streamable-http":
 		startHTTPServer(server)
@@ -74,4 +112,28 @@ func startHTTPServer(s *mcp.Server) {
 		log.Fatalf("HTTP server error %v", err)
 	}
 
+}
+
+func handleCalculate(ctx context.Context, req *mcp.CallToolRequest, param CalculateParams) (*mcp.CallToolResult, CalculateResult, error) {
+	if err := param.Validate(); err != nil {
+		return &mcp.CallToolResult{IsError: true,
+				Content: []mcp.Content{&mcp.TextContent{Text: fmt.Sprintf("Invalid parameters: %v", err)}}},
+			CalculateResult{}, fmt.Errorf("invalid parameters: %v", err)
+	}
+
+	var result float32
+	switch param.Operation {
+	case "add":
+		result = param.Num1 + param.Num2
+	case "subtract":
+		result = param.Num1 - param.Num2
+	case "multiply":
+		result = param.Num1 * param.Num2
+	case "divide":
+		result = param.Num1 / param.Num2
+	}
+
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{&mcp.TextContent{Text: fmt.Sprintf("Result: %f", result)}},
+	}, CalculateResult{Result: result}, nil
 }
